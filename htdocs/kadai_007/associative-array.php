998c0818eb5992bddf30492edd55387deb3be013
yuuchans <159174333+yuuchans@users.noreply.github.com>
2024-04-15 01:22:27 +0900

    Add files via upload

---
+++ htdocs/kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <!-- PHPの埋め込み -->
+    <?php
+
+$goods_date = ['name' => 'onion', 'price' => 200, 'weight' => 160, ];
+
+print_r($goods_date);
+    ?>
+</body>
+
+</html>
